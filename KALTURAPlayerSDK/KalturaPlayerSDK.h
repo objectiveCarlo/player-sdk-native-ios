b9dbe504dad3290349faced3fded0e60b14f9982
Eliza Sapir <eliza.sapir@kaltura.com>
2016-05-02 15:37:28 +0300

    #SUP-7838 #switching to framework & attaching swift bridge & class for chromecast

---
+++ KALTURAPlayerSDK/KalturaPlayerSDK.h
@@ -0,0 +1,21 @@
+//
+//  KalturaPlayerSDK.h
+//  KalturaPlayerSDK
+//
+//  Created by Eliza Sapir on 01/05/2016.
+//  Copyright © 2016 Kaltura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KalturaPlayerSDK.
+FOUNDATION_EXPORT double KalturaPlayerSDKVersionNumber;
+
+//! Project version string for KalturaPlayerSDK.
+FOUNDATION_EXPORT const unsigned char KalturaPlayerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KalturaPlayerSDK/PublicHeader.h>
+
+#import <KalturaPlayerSDK/KPViewController.h>
+#import <KalturaPlayerSDK/KPLog.h>
+#import <KalturaPlayerSDK/KPPlayerConfig.h>
---
+++ KChromecastDeviceControlller.swift
@@ -0,0 +1,18 @@
+//
+//  KChromecastDeviceControlller.swift
+//  KALTURAPlayerSDK
+//
+//  Created by Eliza Sapir on 01/05/2016.
+//  Copyright © 2016 Kaltura. All rights reserved.
+//
+
+import Foundation
+
+class KChromecastDeviceControlller: GCKDeviceScannerListener,
+                                    GCKDeviceManagerDelegate,
+                                    GCKMediaControlChannelDelegate {
+    
+    var scanner: GCKDeviceScanner?
+    
+    
+}
